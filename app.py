5189fa46ae7b48968215c1d1feda109b7a02e478
Arjun Rathinam. R <arjunrathinamtech@gmail.com>
2026-06-18 15:52:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -136,6 +136,7 @@ def view_student(student_id):
     return render_template("view.html", student=student)
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
